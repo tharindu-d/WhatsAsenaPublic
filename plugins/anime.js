cc2f434cb7626a3d9f46dee5a736eab4ad6f727d
tharu99 <54880278+tharu99@users.noreply.github.com>
2021-05-24 07:15:13 +0530

    Update anime.js

--- plugins/anime.js
+++ plugins/anime.js
@@ -103,7 +103,7 @@ if (Config.WORKTYPE == 'private') {
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Made by 𝓙𝓮𝓼𝓼𝓲𝓮 🔥'})
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: '★ 𝙼𝚊𝚍𝚎 𝚋𝚢 𝙹𝚎𝚜𝚜𝚒𝚎 💝🔥'})
 
     }));
 }
@@ -197,7 +197,7 @@ else if (Config.WORKTYPE == 'public') {
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Made by 𝓙𝓮𝓼𝓼𝓲𝓮 🔥'})
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: '★ 𝙼𝚊𝚍𝚎 𝚋𝚢 𝙹𝚎𝚜𝚜𝚒𝚎 💝🔥'})
 
     }));
 }
