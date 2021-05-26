311e6a4c59a247dce43c788af25adc88ec378d6e
tharu99 <54880278+tharu99@users.noreply.github.com>
2021-05-26 11:03:24 +0530

    Add files via upload

---
+++ plugins/covid.js
@@ -0,0 +1,233 @@
+/* Codded by @phaticusthiccy
+Telegram: t.me/phaticusthiccy
+Instagram: www.instagram.com/kyrie.baran
+*/
+
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const Config = require('../config');
+
+const Language = require('../language');
+const Clang = Language.getString('covid');
+
+if (Config.WORKTYPE == 'private') {
+
+	Asena.addCommand({pattern: "covid ?(.*)", fromMe: true, desc: Clang.COV_DESC, dontAddCommandList: true}, (async (message, match) => {
+        if (match[1] === "") {
+            try{
+                //const resp = await fetch("https://coronavirus-19-api.herokuapp.com/all").then(r => r.json());
+                const respo = await got("https://coronavirus-19-api.herokuapp.com/all").then(async ok => {
+                    const resp = JSON.parse(ok.body);
+                    await message.reply(`🌍 *World-Wide Results:*\n🌐 *Total Cases:* ${resp.cases}\n☠️ *Total Deaths:* ${resp.deaths}\n⚕️ *Total Recovered:* ${resp.recovered}\n\nStayhome 🏡 Staysafe..`);
+ 
+                });
+
+            } catch (err) {
+                await message.reply(`Error :\n${err.message}`, MessageType.text)
+            }
+
+        }
+        else if (match[1] === "usa" || match[1] === "Usa" || match[1] === "USA" || match[1] === "america" || match[1] === "America") {
+            try{
+                const respo = await got("https://coronavirus-19-api.herokuapp.com/countries/USA").then(async ok  => {
+                    resp = JSON.parse(ok.body);
+                    await message.reply(`🇺🇲 *Datas for USA:*\n😷 *Total Cases:* ${resp.cases}\n🏥 *Daily Cases:* ${resp.todayCases}\n⚰️ *Total Deaths:* ${resp.deaths}\n☠️ *Daily Deaths:* ${resp.todayDeaths}\n💊 *Total Recovered:* ${resp.recovered}\n😷 *Active Cases:* ${resp.active}\n🆘 *Critical Cases:* ${resp.critical}\n🧪 *Total Test:* ${resp.totalTests}\n\nStayhome 🏡 Staysafe..`);
+
+                });
+
+            } catch (err) {
+                await message.reply(`Error : \n${err.message}`, MessageType.text)
+            }
+        }
+        else if (match[1] === "sl" || match[1] === "Sl" || match[1] === "SL" || match[1] === "SriLanka" || match[1] === "Srilanka" || match[1].includes('srilanka') ) {
+            try{
+                const respo = await got("https://coronavirus-19-api.herokuapp.com/countries/Sri%20Lanka").then(async ok  => {
+                    resp = JSON.parse(ok.body);
+                    await message.reply(`🇱🇰 *Datas for SRILANKA:*\n😷 *Total Cases:* ${resp.cases}\n🏥 *Daily Cases:* ${resp.todayCases}\n⚰️ *Total Deaths:* ${resp.deaths}\n☠️ *Daily Deaths:* ${resp.todayDeaths}\n💊 *Total Recovered:* ${resp.recovered}\n😷 *Active Cases:* ${resp.active}\n🆘 *Critical Cases:* ${resp.critical}\n🧪 *Total Test:* ${resp.totalTests}\n\nStayhome 🏡 Staysafe..`);
+
+                });
+
+            } catch (err) {
+                await message.reply(`Error : \n${err.message}`, MessageType.text)
+            }
+        }
+        else if (match[1] === "uk" || match[1] === "Uk" || match[1] === "UK" || match[1] === "United" || match[1].includes('kingdom') ) {
+            try{
+                const respo = await got("https://coronavirus-19-api.herokuapp.com/countries/UK").then(async ok  => {
+                    resp = JSON.parse(ok.body);
+                    await message.reply(`🇬🇧 *Datas for UK:*\n😷 *Total Cases:* ${resp.cases}\n🏥 *Daily Cases:* ${resp.todayCases}\n⚰️ *Total Deaths:* ${resp.deaths}\n☠️ *Daily Deaths:* ${resp.todayDeaths}\n💊 *Total Recovered:* ${resp.recovered}\n😷 *Active Cases:* ${resp.active}\n🆘 *Critical Cases:* ${resp.critical}\n🧪 *Total Test:* ${resp.totalTests}\n\nStayhome 🏡 Staysafe..`);
+
+                });
+
+            } catch (err) {
+                await message.reply(`Error : \n${err.message}`, MessageType.text)
+            }
+        }
+        else if (match[1] === "in" || match[1] === "ın" || match[1] === "In" || match[1] === "İn" || match[1] === "IN" ||  match[1] === "İN" || match[1] === "india" || match[1] === "India" || match[1].includes('indian') ) {
+            try{
+                const respo = await got("https://coronavirus-19-api.herokuapp.com/countries/India").then(async ok  => {
+                    resp = JSON.parse(ok.body);
+                    await message.reply(`🇮🇳 *भारत के लिए डेटा (INDIA):*\n😷 *Total Cases:* ${resp.cases}\n🏥 *Daily Cases:* ${resp.todayCases}\n⚰️ *Total Deaths:* ${resp.deaths}\n☠️ *Daily Deaths:* ${resp.todayDeaths}\n💊 *Total Recovered:* ${resp.recovered}\n😷 *Active Cases:* ${resp.active}\n🆘 *Critical Cases:* ${resp.critical}\n🧪 *Total Test:* ${resp.totalTests}\n\nStayhome 🏡 Staysafe..`);
+
+                });
+
+            } catch (err) {
+                await message.reply(`Error : \n${err.message}`, MessageType.text)
+            }
+        }
+        else if (match[1] === "cn" || match[1] === "Cn" || match[1] === "CN" || match[1].includes('china') ) {
+            try{
+                const respo = await got("https://coronavirus-19-api.herokuapp.com/countries/China").then(async ok  => {
+                    resp = JSON.parse(ok.body);
+                    await message.reply(`🇨🇳 *Datas for China:*\n😷 *Total Cases:* ${resp.cases}\n🏥 *Daily Cases:* ${resp.todayCases}\n⚰️ *Total Deaths:* ${resp.deaths}\n☠️ *Daily Deaths:* ${resp.todayDeaths}\n💊 *Total Recovered:* ${resp.recovered}\n😷 *Active Cases:* ${resp.active}\n🆘 *Critical Cases:* ${resp.critical}\n🧪 *Total Test:* ${resp.totalTests}\n\nStayhome 🏡 Staysafe..`);
+
+                });
+
+            } catch (err) {
+                await message.reply(`Error : \n${err.message}`, MessageType.text)
+            }
+        }
+        else if (match[1] === "jp" || match[1] === "Jp" || match[1] === "JP" || match[1].includes('japan') ) {
+            try{
+                const respo = await got("https://coronavirus-19-api.herokuapp.com/countries/Japan").then(async ok  => {
+                    resp = JSON.parse(ok.body);
+                    await message.reply(`🇯🇵 *Datas for Japan:*\n😷 *Total Cases:* ${resp.cases}\n🏥 *Daily Cases:* ${resp.todayCases}\n⚰️ *Total Deaths:* ${resp.deaths}\n☠️ *Daily Deaths:* ${resp.todayDeaths}\n💊 *Total Recovered:* ${resp.recovered}\n😷 *Active Cases:* ${resp.active}\n🆘 *Critical Cases:* ${resp.critical}\n🧪 *Total Test:* ${resp.totalTests}\n\nStayhome 🏡 Staysafe..`);
+
+                });
+ 
+            } catch (err) {
+                await message.reply(`Error : \n${err.message}`, MessageType.text)
+            }
+        }
+        else if (match[1] === "ru" || match[1] === "Ru" || match[1] === "RU" || match[1].includes('russia') ) {
+            try{
+                const respo = await got("https://coronavirus-19-api.herokuapp.com/countries/Russia").then(async ok  => {
+                    resp = JSON.parse(ok.body);
+                    await message.reply(`🇷🇺 *Datas for Russia:*\n😷 *Total Cases:* ${resp.cases}\n🏥 *Daily Cases:* ${resp.todayCases}\n⚰️ *Total Deaths:* ${resp.deaths}\n☠️ *Daily Deaths:* ${resp.todayDeaths}\n💊 *Total Recovered:* ${resp.recovered}\n😷 *Active Cases:* ${resp.active}\n🆘 *Critical Cases:* ${resp.critical}\n🧪 *Total Test:* ${resp.totalTests}\n\nStayhome 🏡 Staysafe..`);
+
+                });
+
+            } catch (err) {
+                await message.reply(`Error : \n${err.message}`, MessageType.text)
+            }
+        } 
+        else {
+            return await message.client.sendMessage(
+                message.jid,
+                Clang.NOT,
+                MessageType.text
+            );
+        }
+    }));
+
+}
+else if (Config.WORKTYPE == 'public') {
+
+	Asena.addCommand({pattern: "covid ?(.*)", fromMe: true, desc: Clang.COV_DESC, dontAddCommandList: true}, (async (message, match) => {
+        if (match[1] === "") {
+            try{
+                //const resp = await fetch("https://coronavirus-19-api.herokuapp.com/all").then(r => r.json());
+                const respo = await got("https://coronavirus-19-api.herokuapp.com/all").then(async ok => {
+                    const resp = JSON.parse(ok.body);
+                    await message.reply(`🌍 *World-Wide Results:*\n🌐 *Total Cases:* ${resp.cases}\n☠️ *Total Deaths:* ${resp.deaths}\n⚕️ *Total Recovered:* ${resp.recovered}\n\nStayhome 🏡 Staysafe..`);
+ 
+                });
+
+            } catch (err) {
+                await message.reply(`Error :\n${err.message}`, MessageType.text)
+            }
+
+        }
+        else if (match[1] === "usa" || match[1] === "Usa" || match[1] === "USA" || match[1] === "america" || match[1] === "America") {
+            try{
+                const respo = await got("https://coronavirus-19-api.herokuapp.com/countries/USA").then(async ok  => {
+                    resp = JSON.parse(ok.body);
+                    await message.reply(`🇺🇲 *Datas for USA:*\n😷 *Total Cases:* ${resp.cases}\n🏥 *Daily Cases:* ${resp.todayCases}\n⚰️ *Total Deaths:* ${resp.deaths}\n☠️ *Daily Deaths:* ${resp.todayDeaths}\n💊 *Total Recovered:* ${resp.recovered}\n😷 *Active Cases:* ${resp.active}\n🆘 *Critical Cases:* ${resp.critical}\n🧪 *Total Test:* ${resp.totalTests}\n\nStayhome 🏡 Staysafe..`);
+
+                });
+
+            } catch (err) {
+                await message.reply(`Error : \n${err.message}`, MessageType.text)
+            }
+        }
+        else if (match[1] === "sl" || match[1] === "Sl" || match[1] === "SL" || match[1] === "SriLanka" || match[1] === "Srilanka" || match[1].includes('srilanka') ) {
+            try{
+                const respo = await got("https://coronavirus-19-api.herokuapp.com/countries/Sri%20Lanka").then(async ok  => {
+                    resp = JSON.parse(ok.body);
+                    await message.reply(`🇱🇰 *Datas for SRILANKA:*\n😷 *Total Cases:* ${resp.cases}\n🏥 *Daily Cases:* ${resp.todayCases}\n⚰️ *Total Deaths:* ${resp.deaths}\n☠️ *Daily Deaths:* ${resp.todayDeaths}\n💊 *Total Recovered:* ${resp.recovered}\n😷 *Active Cases:* ${resp.active}\n🆘 *Critical Cases:* ${resp.critical}\n🧪 *Total Test:* ${resp.totalTests}\n\nStayhome 🏡 Staysafe..`);
+
+                });
+
+            } catch (err) {
+                await message.reply(`Error : \n${err.message}`, MessageType.text)
+            }
+        }
+        else if (match[1] === "uk" || match[1] === "Uk" || match[1] === "UK" || match[1] === "United" || match[1].includes('kingdom') ) {
+            try{
+                const respo = await got("https://coronavirus-19-api.herokuapp.com/countries/UK").then(async ok  => {
+                    resp = JSON.parse(ok.body);
+                    await message.reply(`🇬🇧 *Datas for UK:*\n😷 *Total Cases:* ${resp.cases}\n🏥 *Daily Cases:* ${resp.todayCases}\n⚰️ *Total Deaths:* ${resp.deaths}\n☠️ *Daily Deaths:* ${resp.todayDeaths}\n💊 *Total Recovered:* ${resp.recovered}\n😷 *Active Cases:* ${resp.active}\n🆘 *Critical Cases:* ${resp.critical}\n🧪 *Total Test:* ${resp.totalTests}\n\nStayhome 🏡 Staysafe..`);
+
+                });
+
+            } catch (err) {
+                await message.reply(`Error : \n${err.message}`, MessageType.text)
+            }
+        }
+        else if (match[1] === "in" || match[1] === "ın" || match[1] === "In" || match[1] === "İn" || match[1] === "IN" ||  match[1] === "İN" || match[1] === "india" || match[1] === "India" || match[1].includes('indian') ) {
+            try{
+                const respo = await got("https://coronavirus-19-api.herokuapp.com/countries/India").then(async ok  => {
+                    resp = JSON.parse(ok.body);
+                    await message.reply(`🇮🇳 *भारत के लिए डेटा (INDIA):*\n😷 *Total Cases:* ${resp.cases}\n🏥 *Daily Cases:* ${resp.todayCases}\n⚰️ *Total Deaths:* ${resp.deaths}\n☠️ *Daily Deaths:* ${resp.todayDeaths}\n💊 *Total Recovered:* ${resp.recovered}\n😷 *Active Cases:* ${resp.active}\n🆘 *Critical Cases:* ${resp.critical}\n🧪 *Total Test:* ${resp.totalTests}\n\nStayhome 🏡 Staysafe..`);
+
+                });
+
+            } catch (err) {
+                await message.reply(`Error : \n${err.message}`, MessageType.text)
+            }
+        }
+        else if (match[1] === "cn" || match[1] === "Cn" || match[1] === "CN" || match[1].includes('china') ) {
+            try{
+                const respo = await got("https://coronavirus-19-api.herokuapp.com/countries/China").then(async ok  => {
+                    resp = JSON.parse(ok.body);
+                    await message.reply(`🇨🇳 *Datas for China:*\n😷 *Total Cases:* ${resp.cases}\n🏥 *Daily Cases:* ${resp.todayCases}\n⚰️ *Total Deaths:* ${resp.deaths}\n☠️ *Daily Deaths:* ${resp.todayDeaths}\n💊 *Total Recovered:* ${resp.recovered}\n😷 *Active Cases:* ${resp.active}\n🆘 *Critical Cases:* ${resp.critical}\n🧪 *Total Test:* ${resp.totalTests}\n\nStayhome 🏡 Staysafe..`);
+
+                });
+
+            } catch (err) {
+                await message.reply(`Error : \n${err.message}`, MessageType.text)
+            }
+        }
+        else if (match[1] === "jp" || match[1] === "Jp" || match[1] === "JP" || match[1].includes('japan') ) {
+            try{
+                const respo = await got("https://coronavirus-19-api.herokuapp.com/countries/Japan").then(async ok  => {
+                    resp = JSON.parse(ok.body);
+                    await message.reply(`🇯🇵 *Datas for Japan:*\n😷 *Total Cases:* ${resp.cases}\n🏥 *Daily Cases:* ${resp.todayCases}\n⚰️ *Total Deaths:* ${resp.deaths}\n☠️ *Daily Deaths:* ${resp.todayDeaths}\n💊 *Total Recovered:* ${resp.recovered}\n😷 *Active Cases:* ${resp.active}\n🆘 *Critical Cases:* ${resp.critical}\n🧪 *Total Test:* ${resp.totalTests}\n\nStayhome 🏡 Staysafe..`);
+
+                });
+ 
+            } catch (err) {
+                await message.reply(`Error : \n${err.message}`, MessageType.text)
+            }
+        }
+        else if (match[1] === "ru" || match[1] === "Ru" || match[1] === "RU" || match[1].includes('russia') ) {
+            try{
+                const respo = await got("https://coronavirus-19-api.herokuapp.com/countries/Russia").then(async ok  => {
+                    resp = JSON.parse(ok.body);
+                    await message.reply(`🇷🇺 *Datas for Russia:*\n😷 *Total Cases:* ${resp.cases}\n🏥 *Daily Cases:* ${resp.todayCases}\n⚰️ *Total Deaths:* ${resp.deaths}\n☠️ *Daily Deaths:* ${resp.todayDeaths}\n💊 *Total Recovered:* ${resp.recovered}\n😷 *Active Cases:* ${resp.active}\n🆘 *Critical Cases:* ${resp.critical}\n🧪 *Total Test:* ${resp.totalTests}\n\nStayhome 🏡 Staysafe..`);
+
+                });
+
+            } catch (err) {
+                await message.reply(`Error : \n${err.message}`, MessageType.text)
+            }
+        } 
+        else {
+            return await message.client.sendMessage(
+                message.jid,
+                Clang.NOT,
+                MessageType.text
+            );
+        }
+    }));    
+}
