1c20013300fc1f6dc6e2d428e826d9d529fffdf7
tharu99 <54880278+tharu99@users.noreply.github.com>
2021-05-26 11:54:20 +0530

    Update lyrics.js

--- plugins/lyrics.js
+++ plugins/lyrics.js
@@ -18,9 +18,9 @@ Asena.addCommand({pattern: 'lyrics ?(.*)', fromMe: false, desc: Lang.LYRICS_DESC
 		const response = await got(url);
 		const json = JSON.parse(response.body);
 	        if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '*🎙️  ' + Lang.SONG +':* ```' + match[1] + '```\n\n' +
-		'*🎧 ' + Lang.ALBUM +':* ```' + json.result.album + '```\n' +
+		/*'*🎧 ' + Lang.ALBUM +':* ```' + json.result.album + '```\n' +
 		'*🔊 ' + Lang.TITLE +':* ```' + json.result.title + '```\n' +
-		'*🎚️ ' + Lang.PUBLICATION +':* ```' + json.result.artist + '```\n' +
+		'*🎚️ ' + Lang.PUBLICATION +':* ```' + json.result.artist + '```\n' +*/
 		'*🎼 ' + Lang.SONGLI +':* ```' + json.result + '```\n' , MessageType.text);
 		
 	} catch {
