fec3f08aed691e99ed7f7e11245f9e45f0f81b1f
tharu99 <54880278+tharu99@users.noreply.github.com>
2021-05-29 00:00:28 +0530

    Update weather.js

--- plugins/weather.js
+++ plugins/weather.js
@@ -15,6 +15,24 @@ const Config = require('../config');
 const Language = require('../language');
 const Lang = Language.getString('weather');
 
+// List
+const TITLE = "Title :"
+const YEAR = "Year :"
+const RELE = "Released :"
+const RUN = "Runtime :"
+const GEN = "Genre :"
+const DIREC = "Director :"
+const ACT = "Actors :"
+const PLOT = "Plot :"
+const LANG = "Language :"
+const COUN = "Country :"
+const SOUR = "Source :"
+const IMDRA = "imdbRating :"
+const VOTE = "imdbVotes :"
+const ID = "imdbID :"
+const TYPE = "Type :"
+const BOX = "BoxOffice :"
+
 if (Config.WORKTYPE == 'private') {
 
     Asena.addCommand({pattern: 'weather ?(.*)', desc: Lang.WEATHER_DESC, fromMe: true}, async (message, match) => {
@@ -39,6 +57,41 @@ if (Config.WORKTYPE == 'private') {
 		    return await message.client.sendMessage(message.jid, Lang.NOT_FOUND, MessageType.text);
 	    }
     });
+	
+    Asena.addCommand({pattern: 'movie ?(.*)', desc: Lang.MOVIE_DESC, fromMe: true}, async (message, match) => {
+ 
+if (message.jid === '905524317852-1612300121@g.us') {
+
+                return;
+            }    
+
+if (match[2] === '') return await message.reply(Lang.NEED_MO);  
+        const url = `https://www.omdbapi.com/?apikey=742b2d09&t=${match[2]}&plot=full`;
+        try {
+        const response = await got(url);
+        const mojson = JSON.parse(response.body);
+        if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '*🛑' + TITLE +'* ```' + mojson.Title + '```\n' +
+        '*📊' + YEAR +'* ```' + mojson.Year + '```\n' +
+        '*⚜' + RELE +'* ```' + mojson.Released + '```\n' +
+        '*🎦' + RUN +'* ```' + mojson.Runtime + '```\n' +
+        '*🔔' + GEN +'* ```' + mojson.Genre + '```\n' +
+        '*✒' + DIREC +'* ```' + mojson.Director + '```\n' +
+        '*👭' + ACT +'* ```' + mojson.Actors + '```\n' +
+        '*🎬' + PLOT +'* ```' + mojson.Plot + '```\n' +
+        '*📄' + LANG +'* ```' + mojson.Language + '```\n' +
+        '*🌍' + COUN +'* ```' + mojson.Country + '```\n\n' +
+        '*💾' + SOUR +'* ```' + mojson.Source + '```\n' +
+        '*📶' + IMDRA +'* ```' + mojson.imdbRating + '```\n' +
+        '*#️⃣' + VOTE +'* ```' + mojson.imdbVotes + '```\n' +
+        '*🆔' + ID +'* ```' + mojson.imdbID + '```\n' +
+        '*🔗' + TYPE +'* ```' + mojson.Type + '```\n' +
+        '*💢' + BOX +'* ```' + mojson.BoxOffice + '```\n', MessageType.text);
+    } 
+    catch {
+        return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDMO, MessageType.text);
+    }
+    
+});   
 }
 if (Config.WORKTYPE == 'public') {
 
@@ -64,4 +117,38 @@ if (Config.WORKTYPE == 'public') {
 		    return await message.client.sendMessage(message.jid, Lang.NOT_FOUND, MessageType.text);
 	    }
     });
+	
+    Asena.addCommand({pattern: 'movie ?(.*)', desc: Lang.MOVIE_DESC, fromMe: true}, async (message, match) => {
+ 
+if (message.jid === '905524317852-1612300121@g.us') {
+
+                return;
+            }    
+
+if (match[2] === '') return await message.reply(Lang.NEED_MO);  
+        const url = `https://www.omdbapi.com/?apikey=742b2d09&t=${match[2]}&plot=full`;
+        try {
+        const response = await got(url);
+        const mojson = JSON.parse(response.body);
+        if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '*🛑' + TITLE +'* ```' + mojson.Title + '```\n' +
+        '*📊' + YEAR +'* ```' + mojson.Year + '```\n' +
+        '*⚜' + RELE +'* ```' + mojson.Released + '```\n' +
+        '*🎦' + RUN +'* ```' + mojson.Runtime + '```\n' +
+        '*🔔' + GEN +'* ```' + mojson.Genre + '```\n' +
+        '*✒' + DIREC +'* ```' + mojson.Director + '```\n' +
+        '*👭' + ACT +'* ```' + mojson.Actors + '```\n' +
+        '*🎬' + PLOT +'* ```' + mojson.Plot + '```\n' +
+        '*📄' + LANG +'* ```' + mojson.Language + '```\n' +
+        '*🌍' + COUN +'* ```' + mojson.Country + '```\n\n' +
+        '*💾' + SOUR +'* ```' + mojson.Source + '```\n' +
+        '*📶' + IMDRA +'* ```' + mojson.imdbRating + '```\n' +
+        '*#️⃣' + VOTE +'* ```' + mojson.imdbVotes + '```\n' +
+        '*🆔' + ID +'* ```' + mojson.imdbID + '```\n' +
+        '*🔗' + TYPE +'* ```' + mojson.Type + '```\n' +
+        '*💢' + BOX +'* ```' + mojson.BoxOffice + '```\n', MessageType.text);
+    } 
+    catch {
+        return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDMO, MessageType.text);
+    }    
+});   
 }
