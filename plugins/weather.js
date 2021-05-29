60d51da475b5a6975f51d8045f4c31118fea7292
tharu99 <54880278+tharu99@users.noreply.github.com>
2021-05-29 10:03:48 +0530

    Update weather.js

--- plugins/weather.js
+++ plugins/weather.js
@@ -86,21 +86,21 @@ if (match[1] === '') return await message.reply(Lang.NEEDCON);
             try{
                 const response = await got(url);
                 const respod = JSON.parse(response.body);
-                if (response.statusCode === 200) return await message.client.sendMessage(message.jid,'*🌐 ' + Lang.DF +':* ```' + match[1] + '```\n\n' +
-                    '*🔔 ' + Lang.TOTCASE +':* ```' + respod.cases + '```\n' + 
-		    '*🏥 ' + Lang.DAYCAS +':* ```' + respod.todayCases + '```\n' +  
-		    '*⚰️ ' + Lang.TOTDETH +':* ```' + respod.deaths + '```\n' +
-		    '*☠️ ' + Lang.DAYDETH +':* ```' + respod.todayDeaths + '```\n\n' +
-		    '*💊 ' + Lang.TOTRE +':* ```' + respod.recovered + '```\n' +
-                    '*😷 ' + Lang.ACTCAS +':* ```' + respod.active + '```\n' + 
-		    '*🆘 ' + Lang.CRICAS +':* ```' + respod.critical + '```\n' +  
-		    '*🧪 ' + Lang.TOTTEST +':* ```' + respod.totalTests + '```\n\n' +
-		    '*🏡 ' + Lang.STAY +':* ```\n', MessageType.text);
+                if (response.statusCode === 200) return await message.client.sendMessage(message.jid,'*🌐' + Lang.DF +':* ```' + match[1] + '```\n\n' +
+                    '*🔔' + Lang.TOTCASE +':* ```' + respod.cases + '```\n' + 
+		    '*🏥' + Lang.DAYCAS +':* ```' + respod.todayCases + '```\n' +  
+		    '*⚰️' + Lang.TOTDETH +':* ```' + respod.deaths + '```\n' +
+		    '*☠️' + Lang.DAYDETH +':* ```' + respod.todayDeaths + '```\n\n' +
+		    '*💊' + Lang.TOTRE +':* ```' + respod.recovered + '```\n' +
+                    '*😷' + Lang.ACTCAS +':* ```' + respod.active + '```\n' + 
+		    '*🆘' + Lang.CRICAS +':* ```' + respod.critical + '```\n' +  
+		    '*🧪' + Lang.TOTTEST +':* ```' + respod.totalTests + '```\n\n' +
+		    '*🏡' + Lang.STAY +'* \n', MessageType.text);
                 }
 		catch {
                 return await message.client.sendMessage(message.jid, Lang.NOT, MessageType.text);
             }     
-));
+});
 }
 if (Config.WORKTYPE == 'public') {
 
@@ -173,19 +173,19 @@ if (match[1] === '') return await message.reply(Lang.NEEDCON);
             try{
                 const response = await got(url);
                 const respod = JSON.parse(response.body);
-                if (response.statusCode === 200) return await message.client.sendMessage(message.jid,'*🌐 ' + Lang.DF +':* ```' + match[1] + '```\n\n' +
-                    '*🔔 ' + Lang.TOTCASE +':* ```' + respod.cases + '```\n' + 
-		    '*🏥 ' + Lang.DAYCAS +':* ```' + respod.todayCases + '```\n' +  
-		    '*⚰️ ' + Lang.TOTDETH +':* ```' + respod.deaths + '```\n' +
-		    '*☠️ ' + Lang.DAYDETH +':* ```' + respod.todayDeaths + '```\n\n' +
-		    '*💊 ' + Lang.TOTRE +':* ```' + respod.recovered + '```\n' +
-                    '*😷 ' + Lang.ACTCAS +':* ```' + respod.active + '```\n' + 
-		    '*🆘 ' + Lang.CRICAS +':* ```' + respod.critical + '```\n' +  
-		    '*🧪 ' + Lang.TOTTEST +':* ```' + respod.totalTests + '```\n\n' +
-		    '*🏡 ' + Lang.STAY +':* ```\n', MessageType.text);
+                if (response.statusCode === 200) return await message.client.sendMessage(message.jid,'*🌐' + Lang.DF +':* ```' + match[1] + '```\n\n' +
+                    '*🔔' + Lang.TOTCASE +':* ```' + respod.cases + '```\n' + 
+		    '*🏥' + Lang.DAYCAS +':* ```' + respod.todayCases + '```\n' +  
+		    '*⚰️' + Lang.TOTDETH +':* ```' + respod.deaths + '```\n' +
+		    '*☠️' + Lang.DAYDETH +':* ```' + respod.todayDeaths + '```\n\n' +
+		    '*💊' + Lang.TOTRE +':* ```' + respod.recovered + '```\n' +
+                    '*😷' + Lang.ACTCAS +':* ```' + respod.active + '```\n' + 
+		    '*🆘' + Lang.CRICAS +':* ```' + respod.critical + '```\n' +  
+		    '*🧪' + Lang.TOTTEST +':* ```' + respod.totalTests + '```\n\n' +
+		    '*🏡' + Lang.STAY +'* \n', MessageType.text);
                 }
 		catch {
                 return await message.client.sendMessage(message.jid, Lang.NOT, MessageType.text);
             }     
-));
+});
 }
